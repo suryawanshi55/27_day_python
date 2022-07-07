e833d4f24535ddbadeb2854c352384ff7e6490c3
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-07 21:41:17 +0530

    Add files via upload

---
+++ day_26.py
@@ -0,0 +1,32 @@
+# a=2;b=4
+# print(a+b)  6
+# a=input()
+# b=input()
+# print(type(a))<class 'int'>
+# print(type(b))<class 'int'>
+# print(a+b)
+# x=int(input())
+# y=int(input())
+# print (x+y)16
+# p=float(input())     4.0
+# q=float(input())     4.5
+# print(p+q)          8.5
+# x,y=[int(r)for r in input().split()]     4 9
+# print(x+y)      13
+# a=5
+# print(a)     5
+# print("%d"%a)     5
+# print('%f'%a)     5.000000
+# print('%.1f'%a)     5.0
+# a=input('anuja')
+# print("my Name is",a)   anuja
+# print("my name is",a,sep="_")
+# print('my name is',int(a))   45
+# print('my name is',float(a))   ValueError: could not convert string to float: ''
+# print("my name is"+''+str(a))  my name is 45
+# print('my name is'+''+a)        anuja 
+                            #    my name is
+# a=int(input('98765432'))
+# print("your phone no is",a)   98765432
+# print('your phone number is',int(a))
+# print("your phone number is",float(a))
